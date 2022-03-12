9888d049453ee6ccf089e591dd8c522076d46ff5
Nathan Scott <Nathan4.Scott@live.uwe.ac.uk>
2022-03-12 19:09:45 +0000

    functional timer and script loader for patient text.

--- Assets/Scripts/Game/TextController.cs
+++ Assets/Scripts/Game/TextController.cs
@@ -2,36 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public class TextController : MonoBehaviour
 {
-
+    public TextAsset script;
+    public Text text_display;
     public Slider slider;
     //the slider can be set with slider.value
     //it has to be a float, between 0 and 1
 
     private float increment;
-    private bool going_up = true;
+    private string filenum;
 
     void Start()
     {
         slider.value = 1f;
+        filenum = "1";
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (slider.value == 0)
         {
-            going_up = true;
-            //increment = get number of characters in string, add 20, and  divide 1 by that
-            //slider.value = increment
+            text_display.text = script.text;
+            increment =  1.0f / ((script.text.Count() + 4) * 10);
+            slider.value = increment;
         }
         else if (slider.value == 1)
         {
-            going_up = false;
             increment = -0.01f;
             slider.value = 0.99f;
+            script = Resources.Load<TextAsset>("Text/test"+filenum);
+            filenum = "2";
         }
         else
         {
