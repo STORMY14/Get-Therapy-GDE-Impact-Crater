81324f7a28601f56ce89228890138bfa80bb5e4f
Elvin Nicholosn <Elvin2.Nicholson@live.uwe.ac.uk>
2022-05-05 00:55:31 +0100

    GAME IS FINISHED (FOR REAL)

--- Assets/Scripts/Game/ThoughtMaker.cs
+++ Assets/Scripts/Game/ThoughtMaker.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -61,7 +59,6 @@ public class ThoughtMaker : MonoBehaviour
         }
         if (loaded)
         {
-            Debug.Log("LOADED");
             vibeCheck.is_therapist_listening = true;
             vibeCheck.is_patient_listening = false;
             ready = false;
--- Assets/Scripts/Game/popper.cs
+++ Assets/Scripts/Game/popper.cs
@@ -1,19 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Text;
-using UnityEditor;
-using UnityEditor.UI;
 using UnityEngine;
 
 public class popper : MonoBehaviour
 {
     public int bubblenum;
-    // Start is called before the first frame update
-    private SpriteRenderer renderer;
-    void Start()
-    {
-        renderer = GetComponent<SpriteRenderer>();
-    }
 
     public void OnPress()
     {
